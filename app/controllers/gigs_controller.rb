56760b22a482a3cde23775f6cd77b7c3866ff7f2
straysheep3 <getloose.kage@gmail.com>
2016-06-21 20:08:22 +0900

    finish

--- app/controllers/gigs_controller.rb
+++ app/controllers/gigs_controller.rb
@@ -1,7 +1,9 @@
 class GigsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :search, :show]
 
   def index
-    @gigs = Gig.all.order("created_at desc").page(params[:page]).per(25)
+    @gigs = Gig.all.order_list(params[:sort_by]).page(params[:page]).per(25)
+    @users = User.all
   end
 
   def show
@@ -16,31 +18,50 @@ class GigsController < ApplicationController
 
   def create
     @gig = Gig.new(gig_params)
+    @gig.user = current_user
     if @gig.save
       redirect_to @gig
     else
+      flash[:error] = @gig.errors.full_messages.to_sentence
       render "new"
     end
   end
 
   def edit
     @gig = Gig.find(params[:id])
+    if current_user != @gig.user
+      flash[:error] = "You are not the owner of this gig"
+      redirect_to @gig
+    end
   end
 
   def update
     @gig = Gig.find(params[:id])
-    @gig.update(gig_params)
+    if current_user == @gig.user
+      @gig.update(gig_params)
+    else
+      flash[:error] = "You are not the owner of this gig"
+    end
     redirect_to @gig
+
   end
 
   def destroy
     @gig = Gig.find(params[:id])
-    @gig.destroy
+    if current_user == @gig.user
+      @gig.destroy
+    else
+      flash[:error] = "You are not the owner of this gig"
+    end
     redirect_to :mygigs
   end
 
   def search
-    @gigs = Gig.search(params).page(params[:page]).per(25)
+    if params[:category].blank? && params[:search].blank?
+      @gigs = Gig.all.order("created_at desc").page(params[:page]).per(25)
+    else
+      @gigs = Gig.search(params).page(params[:page]).per(25)
+    end
   end
 
   def mygigs
